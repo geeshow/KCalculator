4412fc18d17d8efc94abc0b39feca2dce2b732f7
Tei Park <ken2484@gmail.com>
2019-06-18 21:25:42 +0900

    Initial commit

---
+++ app/src/main/java/com/ken207/mygit1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ken207.mygit1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
