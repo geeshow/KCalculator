8c070c7a4814a69a1795c7979486f39bbe0d273d
geeshow <kute80@naver.com>
2019-07-10 18:01:53 +0900

    refactoring

--- app/src/main/java/com/ken207/mygit1/Constants.kt
+++ app/src/main/java/com/ken207/mygit1/Constants.kt
@@ -1,14 +1,14 @@
 package com.ken207.mygit1
 
-const val DIGIT = "."
-const val EQUALS = "="
-const val PLUS = "+"
-const val MINUS = "-"
-const val MULTIPLY = "*"
-const val DIVIDE = "/"
-const val PERCENT = "%"
-const val L_BRACKET = "("
-const val R_BRACKET = ")"
+const val DIGIT = '.'
+const val EQUALS = '='
+const val PLUS = '+'
+const val MINUS = '-'
+const val MULTIPLY = '*'
+const val DIVIDE = '/'
+const val PERCENT = '%'
+const val L_BRACKET = '('
+const val R_BRACKET = ')'
 const val POWER = "power"
 const val ROOT = "root"
 const val DECIMAL = "decimal"
--- app/src/main/java/com/ken207/mygit1/MainActivity.kt
+++ app/src/main/java/com/ken207/mygit1/MainActivity.kt
@@ -24,16 +24,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun clickButton(btnNum: View) {
         when (btnNum.id) {
-            R.id.btn0 -> putNumber("0")
-            R.id.btn1 -> putNumber("1")
-            R.id.btn2 -> putNumber("2")
-            R.id.btn3 -> putNumber("3")
-            R.id.btn4 -> putNumber("4")
-            R.id.btn5 -> putNumber("5")
-            R.id.btn6 -> putNumber("6")
-            R.id.btn7 -> putNumber("7")
-            R.id.btn8 -> putNumber("8")
-            R.id.btn9 -> putNumber("9")
+            R.id.btn0 -> putNumber('0')
+            R.id.btn1 -> putNumber('1')
+            R.id.btn2 -> putNumber('2')
+            R.id.btn3 -> putNumber('3')
+            R.id.btn4 -> putNumber('4')
+            R.id.btn5 -> putNumber('5')
+            R.id.btn6 -> putNumber('6')
+            R.id.btn7 -> putNumber('7')
+            R.id.btn8 -> putNumber('8')
+            R.id.btn9 -> putNumber('9')
             R.id.btnMulti -> putOperator(MULTIPLY)
             R.id.btnMinus -> putOperator(MINUS)
             R.id.btnPlus -> putOperator(PLUS)
@@ -49,16 +49,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     //
-    private fun putNumber(number:String) {
-        if ( getFormulaString() == "0" )
-            setFormula(number)
-        else
-            addFormula(number)
+    private fun putNumber(inputChar:Char) {
+        var idxSelectionStart:Int = txtFormula.getSelectionStart()
+        var idxSelectionEnd = txtFormula.getSelectionEnd()
 
+        if ( idxSelectionStart == 0 ) {
+            if ( inputChar == '0' )
+                return
+        }
+        else {
+            val leftSideChar:Char = txtFormula.text.get(idxSelectionStart - 1)
+            val leftSideInput:Char = inputChar
+
+            if ( leftSideChar == '%' )
+                addFormula(MULTIPLY)
+        }
+
+        addFormula(inputChar)
     }
 
-    private fun putOperator(operator:String) {
-        addFormula(operator)
+    private fun putOperator(operator:Char) {
+        var idxSelectionStart:Int = txtFormula.getSelectionStart()
+        var idxSelectionEnd = txtFormula.getSelectionEnd()
+
+        if ( idxSelectionStart == 0 ) {
+            return // Deny putting operator as a first
+        }
+        else {
+            val leftSideChar:Char = txtFormula.text.get(idxSelectionStart - 1)
+
+            if ( isOperator(leftSideChar) ) {
+                idxSelectionStart--
+            }
+        }
+
+        if ( idxSelectionEnd != txtFormula.text.length ) {
+            val rightSideChar:Char = txtFormula.text.get(idxSelectionEnd)
+
+            if ( isOperator(rightSideChar) ) {
+                idxSelectionEnd++
+            }
+        }
+
+        addFormula(operator, idxSelectionStart, idxSelectionEnd)
     }
 
     private fun setNegative() {
@@ -79,7 +112,7 @@ class MainActivity : AppCompatActivity() {
     private fun setBracket() {
         var idxSelectionStart:Int = txtFormula.getSelectionStart()
         var idxSelectionEnd = txtFormula.getSelectionEnd()
-        var whichBracket:String = L_BRACKET // "("
+        var whichBracket:Char = L_BRACKET // "("
 
         if ( idxSelectionStart == 0 ) {
             whichBracket = L_BRACKET // "("
@@ -89,7 +122,7 @@ class MainActivity : AppCompatActivity() {
 
             if ( isOperator(leftSideChar)  )
                 whichBracket = L_BRACKET // "("
-            else if ( leftSideChar == L_BRACKET[0] )
+            else if ( leftSideChar == L_BRACKET )
                 whichBracket = L_BRACKET // "("
             else
                 whichBracket = R_BRACKET // ")"
@@ -101,7 +134,7 @@ class MainActivity : AppCompatActivity() {
     private fun setPercent() {
         var idxSelectionStart:Int = txtFormula.getSelectionStart()
         var idxSelectionEnd = txtFormula.getSelectionEnd()
-        var whichBracket:String = L_BRACKET // "("
+        var whichBracket:Char = L_BRACKET // "("
 
         if ( idxSelectionStart == 0 ) {
             return
@@ -111,46 +144,25 @@ class MainActivity : AppCompatActivity() {
 
             if ( isOperator(leftSideChar)  )
                 return
-            else if ( leftSideChar == L_BRACKET[0] ) // "("
+            else if ( leftSideChar == L_BRACKET ) // '('
                 return
             else
-                addFormula("%")
+                addFormula('%')
         }
     }
 
     private fun getFormulaString():String {
         return txtFormula.text.toString()
     }
+    private fun addFormula(inputChar:Char, idxSelectionStart:Int=txtFormula.getSelectionStart(), idxSelectionEnd:Int=txtFormula.getSelectionEnd()) {
+        var moveToSelectionStart:Int = txtFormula.getSelectionStart()
+        var moveToSelectionEnd = txtFormula.getSelectionEnd()
 
-    private fun addFormula(addStr:String) {
-        var idxSelectionStart:Int = txtFormula.getSelectionStart()
-        var idxSelectionEnd = txtFormula.getSelectionEnd()
-
-        if ( idxSelectionStart == 0 ) {
-            if ( !checkAllowCharAtFirst(addStr.first()) )
-                return
-        }
-        else {
-            val leftSideChar:Char = txtFormula.text.get(idxSelectionStart - 1)
-            val leftSideInput:Char = addStr.first()
-
-            if ( isOperator(leftSideChar) && isOperator(leftSideInput) ) {
-                idxSelectionStart--
-            }
-        }
-
-        if ( idxSelectionEnd != txtFormula.text.length ) {
-            val rightSideChar:Char = txtFormula.text.get(idxSelectionEnd)
-            val rightSideInput:Char = addStr.last()
-
-            if ( isOperator(rightSideChar) && isOperator(rightSideInput) ) {
-                idxSelectionEnd++
-            }
-        }
-
-        txtFormula.getText().replace(idxSelectionStart, idxSelectionEnd, addStr)
-        txtFormula.setSelection(idxSelectionStart+addStr.length,idxSelectionStart+addStr.length)
+        txtFormula.getText().replace(moveToSelectionStart, moveToSelectionEnd, inputChar.toString())
+        txtFormula.setSelection(moveToSelectionStart+1,moveToSelectionStart+1)
     }
+
+
 
     private fun setFormula(char:String) {
         // TODO SpannableString
@@ -159,14 +171,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkAllowCharAtFirst(char:Char):Boolean {
-        if ( char.toString() in setOf(MULTIPLY, DIVIDE, PLUS, MINUS, R_BRACKET, "0"))
+        if ( char in setOf(MULTIPLY, DIVIDE, PLUS, MINUS, R_BRACKET, PERCENT))
             return false
 
         return true
     }
 
     private fun isOperator(char:Char):Boolean {
-        if ( char.toString() in setOf(MULTIPLY, DIVIDE, PLUS, MINUS))
+        if ( char in setOf(MULTIPLY, DIVIDE, PLUS, MINUS))
             return true
 
         return false
